7e4871433fd37431edab09516957ffc8ba292206
Gino-Y <ginoyan250@gmail.com>
2021-12-03 17:34:29 +0800

    feat:request封装

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,6 +3,17 @@ import Welcome from './Welcome.vue'
 export default {
   name:'login',
   components:{Welcome},
+  mounted() {
+    this.$request({
+      method:'get',
+      url:'/login',
+      data:{
+        name:'jack'
+      }
+    }).then((res)=>{
+      console.log(res)
+    })
+  },
   methods:{
     gouHome(){
       this.$router.push('/welcome')
@@ -13,7 +24,6 @@ export default {
 
 <template>
   <h1>欢迎来到登陆界面</h1>
-  <Welcome msg="jack"/>
   <el-button @click="gouHome">回首页</el-button>
 </template>
 
